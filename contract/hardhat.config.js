5b26e1d1d79038c2d06ef23461527313aa9d2752
Tashrif2001 <tashrifpro@gmail.com>
2024-09-07 17:02:52 -0400

    sepolia alchemy account linked, transaction deployed

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+require('@nomiclabs/hardhat-waffle')
+
 module.exports = {
-  solidity: "0.8.24",
-};
+  solidity: '0.8.0',
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/OTc8PASzK3YUTgXm-_lqADxgK79Z1k4A',
+      accounts: [ 'be1a262badbecaa77f10f0ed461a72ad0b1f907eb2dbdcf79d41a182d8eb5b49' ]
+    }
+  }
+}
