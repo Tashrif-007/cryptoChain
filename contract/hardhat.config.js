9eb3c8e86147ad615575b93c3c402822f3b5237f
Tashrif2001 <tashrifpro@gmail.com>
2024-09-07 17:06:00 -0400

    env var included

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,13 +1,15 @@
 
 
 require('@nomiclabs/hardhat-waffle')
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const URL = process.env.URL;
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     sepolia: {
-      url: 'https://eth-sepolia.g.alchemy.com/v2/OTc8PASzK3YUTgXm-_lqADxgK79Z1k4A',
-      accounts: [ 'be1a262badbecaa77f10f0ed461a72ad0b1f907eb2dbdcf79d41a182d8eb5b49' ]
+      url: URL,
+      accounts: [ PRIVATE_KEY ]
     }
   }
 }
