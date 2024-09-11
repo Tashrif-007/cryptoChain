37045e9ebc83f1c8c5381222c48c87901ffca5e3
Tashrif2001 <tashrifpro@gmail.com>
2024-09-11 07:38:40 -0400

    transaction done and services component done

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -18,6 +18,8 @@ export const TransactionProvider = ({ children }) => {
 
     const [currentAccount, setCurrentAccount] = useState("");
     const [formData, setFormData] = useState({ addressTo: '', amount: '', keyword: '', message: ''});
+    const [isLoading, setIsLoading] = useState(false);
+    const [transactionCount, setTransactionCount] = useState(localStorage.getItem('transactionCount'));
 
     const handleChange = (e,name) => {
         setFormData((prevState) => ({...prevState, [name]: e.target.value}))
@@ -75,6 +77,17 @@ export const TransactionProvider = ({ children }) => {
                     value: parsedAmount._hex,
                 }]
             })
+
+            const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword);
+
+            setIsLoading(true);
+            console.log(`Loading - ${transactionHash.hash}`);
+            await transactionHash.wait()
+            setIsLoading(false);
+            console.log(`Success - ${transactionHash.hash}`);
+
+            const transactionCount = await transactionContract.getTransactionCount();
+            setTransactionCount(transactionCount.toNumber())
         } catch (error) {
             console.log(error);
             
