bb94f0c0a6685cf1bc9d8b0af4d14676ee3fbcd1
Tashrif2001 <tashrifpro@gmail.com>
2024-09-12 05:54:59 -0400

    env var update

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -2,7 +2,7 @@
 
 require('@nomiclabs/hardhat-waffle')
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const URL = process.env.URL;
+const URL = process.env.P_URL;
 
 module.exports = {
   solidity: '0.8.0',
